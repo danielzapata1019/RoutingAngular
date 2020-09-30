b6b2f209fe3f37fc7f524d5234a03d956fa5d155
DANIEL ALBERTO ZAPATA SOTO <dzapatso@contratista.epm.co>
2020-09-30 08:40:36 -0500

    Routing Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PruebaComponent } from './prueba/prueba.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prueba',
+    component : PruebaComponent
+  },
+  {
+    path:'modulo',
+    loadChildren:()=> import('./modules/modules.module')
+    .then(m => m.ModulesModule)
+  },
+  {
+    path:'**',
+    redirectTo:'https://www.google.com.co',
+    pathMatch:'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { ModulesModule} from './modules/modules.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModulesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
